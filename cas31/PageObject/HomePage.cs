c040ca05e573aabc58e9af076dc12fc2a21bb489
Slavenka2112 <62516799+Slavenka2112@users.noreply.github.com>
2020-05-23 19:10:48 +0200

    Add project files.

---
+++ cas31/PageObject/HomePage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+
+namespace cas31.PageObject
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+        public void GoToPage()
+        {
+            this.driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+        public IWebElement SearchField
+
+        {
+            get
+            {
+                IWebElement element;
+                try
+                {
+                    element = this.driver.FindElement(By.Name("q"));
+                }catch (Exception)
+                {
+                    element = null;
+                }
+                return element;
+            }
+        }
+        public ResultsPages SearchFor(string search)
+        {
+            //this.driver.FindElement(By.Name("q").SendKeys(search);
+
+            this.SearchField?.SendKeys(search);
+            this.SearchField?.Submit();
+            wait.Until(EC.ElementIsVisible(By.Id("result-stats")));
+            return new ResultsPages(this.driver);
+        }
+        public IWebElement PrivacyLink
+        {
+            get
+            {
+                    IWebElement element;
+                    try
+                    {
+                        element = this.driver.FindElement(By.XPath("//a[contains@href, 'https://policies.google.com/privacy')]"));
+                    }
+                    catch (Exception)
+                    {
+                        element = null;
+                    }
+                    return element;
+            }
+        }
+            public void ClickOnPrivacy()
+        {
+            this.PrivacyLink?.Click();
+        }
+        
+    }
+}
---
+++ cas31/TestClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using cas31.PageObject;
+
+
+namespace cas31
+{
+    class TestClass
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+
+        }
+        [Test]
+        public void TestGoogleSearch()
+        {
+            HomePage naslovna = new HomePage(driver);
+            ResultsPages rezultati;
+
+            naslovna.GoToPage();
+            rezultati = naslovna.SearchFor("Csharp Selenium PageObject Model");
+        
+                Assert.Greater(rezultati.NumberOfResults, 0);
+                }
+        [Test]
+        public void TestClickOnPrivacy()
+        {
+            HomePage naslovna = new HomePage(driver);
+            naslovna.GoToPage();
+            naslovna.ClickOnPrivacy();
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
